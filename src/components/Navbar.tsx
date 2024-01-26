53f62b15091a0aece1ef3c31e25ae8aa0733d6ed
Simone <150595274+simonetimi@users.noreply.github.com>
2024-01-26 23:16:17 +0100

    feat: add basic structure for Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+function Navbar() {
+  return (
+    <nav>
+      <button>Home</button>
+      <button>Men&apos;s Wear</button>
+      <button>Women&apos;s Wear</button>
+      <button className="material-symbols-outlined">shopping_cart</button>
+    </nav>
+  );
+}
+
+export default Navbar;
